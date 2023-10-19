33380e8da6e386d78a54de48a6d1cb098c04451a
Cloud-PR <polaposa.1@gmail.com>
2023-10-19 11:15:39 -0500

    TheNameisPaola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi my name is paolagit");
+        System.out.println("hi my name is paola");
         }
 
 
