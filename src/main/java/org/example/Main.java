417ddf6fb5fb794878aca3ac1ebc60fda7b70231
Neha Udeshi <nehaudeshi@Nehas-Air.attlocal.net>
2023-10-19 11:56:28 -0500

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,39 @@
 package org.example;
 
-import org.w3c.dom.ls.LSOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Heyyyyyy");
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Welcome to the online store");
+            System.out.println("Select which option you would like to choose:");
+            System.out.println("[1] View products");
+            System.out.println("[2] View cart");
+            System.out.println("[3] Exit");
 
-        System.out.println("hi my name is paola");
+            try {
+                int choice = scanner.nextInt();
+                scanner.nextLine();
 
-        System.out.println("hola");
-        System.out.println("steeven");
-
-
-        System.out.println("Hello and welcome!");
-        System.out.println("Hi my name is neha");
-
-
-
+                switch (choice) {
+                    case 1:
+                        System.out.println("display products method will go here");
+                        break;
+                    case 2:
+                        System.out.println("display cart method will go here");
+                        break;
+                    case 3:
+                        System.out.println("We hope you come back soon! Thank you.");
+                        System.exit(0);
+                    default:
+                        System.out.println("Sorry, please enter 1, 2, or 3.");
+                }
+            } catch (Exception ex) {
+                System.out.println("Sorry, please enter 1, 2, or 3.");
+                scanner.nextLine();
+            }
         }
-
-
     }
-
-        }
-
-    }
-
+}
