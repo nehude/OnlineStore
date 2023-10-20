5930530ec7c1020c9b0c9c59755f6375702f4b00
Tatieonna McNeese <tatieonnamcneese@Tatieonnas-Air.attlocal.net>
2023-10-20 11:20:08 -0500

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,18 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Heyyyyyy");
 
-
-        System.out.println("hi my name is paola");
-
-        System.out.println("hola");
-        System.out.println("steeven");
-
-
-        System.out.println("Hello and welcome!");
-
-        
 
 
         }
